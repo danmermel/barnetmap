1b2bd907863f4f2eb06ec93d47745a4c8f03361d
Daniel Mermelstein <danmermel@gmail.com>
2026-03-11 12:12:37 +0000

    removing a lambda functio we are not using
    Signed-off-by: Daniel Mermelstein <danmermel@gmail.com>

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,14 +1,4 @@
 # barnetmap
-module "barnetmap" {
-  source        = "./modules/apicall"
-  function_name = "barnetmap"
-  role          = aws_iam_role.barnetmapLambdaRole.arn
-}
-
-
-output "barnetmapFunctionUrl" {
-  value = module.barnetmap.url
-}
 
 module "barnetgeojson" {
   source        = "./modules/apicall"
