d0abd23f662f344876fbd6490f3d9ae0c49ff30e
Daniel Mermelstein <danmermel@gmail.com>
2026-03-15 08:41:07 +0000

    getting the front end to work
    Signed-off-by: Daniel Mermelstein <danmermel@gmail.com>

--- lambda/barnetmem.js
+++ lambda/barnetmem.js
@@ -1,5 +1,5 @@
 const { DatabaseSync } = require('node:sqlite')
-const database = new DatabaseSync('gvi.sqlite', { "readOnly": true })
+const database = new DatabaseSync('mem.sqlite', { "readOnly": true })
 const headers = { "Content-type": "application/json" }
 
 
@@ -7,11 +7,11 @@ const handler = async function (event, context) {
   ///console.log("params ", params)
   //console.log("preparing statement")
   try {
-    console.log("querying..")
+    //console.log("querying..")
     const query = database.prepare(`select mem.postcode, member_count, geojson from mem inner join geo on mem.postcode = geo.postcode`)
     var ret = query.all()
     
-    console.log("query success: ", ret)
+    //console.log("query success: ")
     //we create one single geojson object with all the postcodes in it, so that it can be rendered directly on a map
     const fc = {
       "type": "FeatureCollection",
@@ -22,17 +22,18 @@ const handler = async function (event, context) {
     let totalmembers = 0 
 
     //
-    console.log("making properties")
+    //console.log("making properties")
     for (r of ret) {
       newgeo = JSON.parse(r.geojson)
       newgeo.properties.member_count = r.member_count
       fc.features.push(newgeo)
+      totalmembers += r.member_count
     }
 
     console.log("totalmembers ", totalmembers)
     // make sure that all the total members are a number between 1-100
     for (feature of fc.features) {
-      feature.member_percentage = (feature.member_count / totalmembers) * 100
+      feature.properties.member_percentage = (feature.properties.member_count / totalmembers) * 100
     }
 
     return {
